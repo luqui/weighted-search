a06774627225cd4b2a7488ea31da19aad6c97fe6
Luke Palmer <luqui@clozure.com>
2013-02-05 08:28:22 -0700

    Improved documentation.

--- Control/Monad/WeightedSearch.hs
+++ Control/Monad/WeightedSearch.hs
@@ -1,27 +1,52 @@
+-- | This is a nondeterminism monad which allows you to give computations
+-- weights, such that the lowest-weight computations will be returned first.
+-- This allows you to search infinite spaces productively, by guarding
+-- recursive calls with weights.  Example:
+--
+-- > import qualified Control.Monad.WeightedSearch as W
+-- > import Control.Applicative
+-- > 
+-- > -- All naturals, weighted by the size of the number
+-- > naturals :: W.T Integer Integer
+-- > naturals = go 0
+-- >     where
+-- >     go n = pure n <|> W.weight 1 (go $! n+1)
+-- > 
+-- > -- All finite lists, weighted by the length of the list
+-- > finiteLists :: W.T Integer a -> W.T Integer a
+-- > finiteLists = pure [] <|> W.weight 1 ((:) <$> w <*> finiteLists w)
+-- > 
+-- > -- A list of all finite lists of naturals
+-- > finiteListsOfNaturals = W.toList (finiteLists naturals)
+-- >    -- [ [], [0], [0,0], [1], [0,0,0], [0,1], [1,0], [2], [0,0,0,0], [0,0,1], ... ]
+--
+-- Weights must be strictly positive for this to be well-defined.
+
 module Control.Monad.WeightedSearch 
-    ( M, Weight(..), weight )
+    ( T, Weight(..), weight, toList )
 where
 
 import Control.Applicative
 import Control.Monad (ap, MonadPlus(..))
 import Control.Arrow (first)
 import Data.Ratio (Ratio)
-import Data.Foldable (Foldable, foldMap)
+import Data.Foldable (Foldable, foldMap, toList)
 import Data.Traversable (Traversable, sequenceA)
 import Data.Monoid (Monoid(..))
 
 -- | Weighted nondeterminstic computations over the weight @w@.  
-data M w a
+data T w a
     = Fail
-    | Yield a (M w a)
-    | Weight w (M w a)
+    | Yield a (T w a)
+    | Weight w (T w a)
 
--- | The class of positive weights. We need to know how to subtract.
+-- | The class of positive weights. We need to know how to subtract.  Weights
+-- must be strictly positive.
 class (Ord w) => Weight w where
     difference :: w -> w -> w
 
--- | Take a *positive* weight and weight a computation with it.
-weight :: w -> M w a -> M w a
+-- | Take a positive weight and weight a computation with it.
+weight :: w -> T w a -> T w a
 weight = Weight
 
 instance Weight Int where difference = (-)
@@ -30,18 +55,18 @@ instance Weight Float where difference = (-)
 instance Weight Double where difference = (-)
 instance (Integral a) => Weight (Ratio a) where difference = (-)
 
-instance Functor (M w) where
+instance Functor (T w) where
     fmap _ Fail = Fail
     fmap f (Yield x w) = Yield (f x) (fmap f w)
     fmap f (Weight a w) = Weight a (fmap f w)
 
-instance (Weight w) => Monad (M w) where
+instance (Weight w) => Monad (T w) where
     return x = Yield x Fail
     Fail >>= _ = Fail
     Yield x m >>= f = f x `mplus` (m >>= f)
     Weight w m >>= f = Weight w (m >>= f)
 
-instance (Weight w) => MonadPlus (M w) where
+instance (Weight w) => MonadPlus (T w) where
     mzero = Fail
     Fail `mplus` m = m
     Yield x m `mplus` n = Yield x (m `mplus` n)
@@ -53,16 +78,20 @@ instance (Weight w) => MonadPlus (M w) where
             EQ -> Weight w (m `mplus` n)
             GT -> Weight w' (Weight (difference w w') m `mplus` n)
 
-instance (Weight w) => Applicative (M w) where
+instance (Weight w) => Applicative (T w) where
     pure = return
     (<*>) = ap
 
-instance Foldable (M w) where
+instance (Weight w) => Alternative (T w) where
+    empty = mzero
+    (<|>) = mplus
+
+instance Foldable (T w) where
     foldMap _ Fail = mempty
     foldMap f (Yield a ms) = f a `mappend` foldMap f ms
     foldMap f (Weight _ w) = foldMap f w
 
-instance Traversable (M w) where
+instance Traversable (T w) where
     sequenceA Fail = pure Fail
     sequenceA (Yield x w) = Yield <$> x <*> sequenceA w
     sequenceA (Weight a w) = Weight a <$> sequenceA w
